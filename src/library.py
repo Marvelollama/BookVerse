d946db1bd220c125987be7ab6e6507cca986e6f9
Kasturi Sinha <sinha.kasturi.05@gmail.com>
2025-04-15 19:47:05 +0530

    added library class

---
+++ src/library.py
@@ -0,0 +1,23 @@
+class Library:
+    def __init__(self):
+        self.books = dict()
+        self.members = dict()
+        
+    def add_book(self, book):
+        self.books[book.isbn] = book
+    
+    def remove_book(self, book):
+        self.books.remove(book.isbn)
+        
+    def register_member(self, member):
+        self.members[member.id] = member
+        
+    def search_book(self, book):
+        if book.isbn in self.books:
+            return self.books[book.isbn]
+        else:
+            print(f"{book.title} is unavailable")
+    
+    def __getitem__(self, isbn):
+        return self.books.get(isbn, None)
+    
