e89d38466e7e63bc2d1b2742a43b5743f8da4657
Kasturi Sinha <sinha.kasturi.05@gmail.com>
2025-04-15 19:15:18 +0530

    added Book class

---
+++ src/book.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, author, isbn, copies): # should i add type hints?
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.copies = copies
+    
+    def __str__(self):
+        return f"{self.title} is written by {self.author} (ISBN: {self.isbn}) and has {self.copies} left"
+    
+    def __add__(self, num):
+        return Book(self.copies + num)
+    
+    def __sub__(self, num):
+        return Book(self.copies - num)
+    
